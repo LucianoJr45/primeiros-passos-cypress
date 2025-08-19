de3f6b9076e15a8b4de64b631700adc7365199c6
unknown <lucianojunioraguiarfraga@gmail.com>
2025-08-19 19:33:19 -0300

    add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,43 +1,49 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
 
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    
+    
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: '.--close',
     submitButton: "[type='submit']",
+    genericCombobox: ".oxd-select-text--arrow"
     
   }
 
 
 
-  it.only('Login - success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.genericField).eq(4).clear().type('empIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('driverIdTest1')
-    cy.get('.oxd-input-field-bottom-space').eq(4).clear().type('idTest')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2025-08-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close').click()
+  it.only('User Info Update - success', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
+    
+
+    
+    // cy.get(selectorsList.genericField).eq(4).clear().type('empIdTest')
+    // cy.get(selectorsList.genericField).eq(5).clear().type('driverIdTest1')
+    // cy.get('.oxd-input-field-bottom-space').eq(4).clear().type('idTest')
+    // cy.get(selectorsList.dateField).eq(0).clear().type('2025-08-10')
+    // cy.get(selectorsList.dateCloseButton).click()
+    // cy.get(selectorsList.genericCombobox).eq(0).click()
+    // cy.contains('Brazilian').click()
+    // cy.get(selectorsList.genericCombobox).eq(1).click()
+    // cy.contains('Married').click()
+    // cy.get(selectorsList.submitButton).eq(0).click()
+    // cy.get('body').should('contain', 'Successfully Updated')
+    // cy.get('.oxd-toast-close').click()
     
 
   })
